44a968f670af74f99376e85a0a336fb85c526b8a
Zhenya Igorevna <z.nelyub@gmail.com>
2015-02-19 15:22:51 +0300

    a

---
+++ src/main/java/tets/NewMain.java
@@ -0,0 +1,10 @@
+package tets;
+
+/**
+ * Created by Yauheniya_Neliub on 2/19/2015.
+ */
+public class NewMain {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
